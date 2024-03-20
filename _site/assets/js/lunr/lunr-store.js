431e3d89cbed65670a76ac269db6a01961d44835
Louis-Goldford <louisgoldford@Louis-Goldfords-MacBook-Pro.local>
2024-03-20 23:02:19 +0100

    trying again

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -42,56 +42,59 @@ var store = [{
         "teaser": null
       },{
     "title": "Page Not Found!",
-    "excerpt":"Sorry, but the page you were trying to view does not exist.            ","url": "http://localhost:4000/404.html"
+    "excerpt":"Sorry, but the page you were trying to view does not exist.            ","url": "/404.html"
   },{
     "title": "About",
-    "excerpt":"Tempor velit sint sunt ipsum tempor enim ad qui ullamco. Est dolore anim ad velit duis dolore minim sunt aliquip amet commodo labore. Ut eu pariatur aute ea aute excepteur laborum. Esse ea esse excepteur minim mollit qui cillum excepteur ex dolore magna. Labore deserunt fugiat incididunt incididunt sint ea....","url": "http://localhost:4000/about/"
+    "excerpt":"Tempor velit sint sunt ipsum tempor enim ad qui ullamco. Est dolore anim ad velit duis dolore minim sunt aliquip amet commodo labore. Ut eu pariatur aute ea aute excepteur laborum. Esse ea esse excepteur minim mollit qui cillum excepteur ex dolore magna. Labore deserunt fugiat incididunt incididunt sint ea....","url": "/about/"
   },{
     "title": "Posts by Category",
-    "excerpt":" ","url": "http://localhost:4000/categories/"
+    "excerpt":" ","url": "/categories/"
   },{
     "title": "Final Exam: Mixed Piece with Max Patch",
-    "excerpt":"Deadline: Week 13: 16 Jan. 1. Overview The final exam is a composition for instrument(s) with electronics realized in MaxMSP with occasional checkpoints where I evaluate your progress throughout the semester. For example, our Midterm Exam is one such checkpoint. Your project may be conceived as a work-in-progress but must...","url": "http://localhost:4000/final-mixed-piece/"
+    "excerpt":"Deadline: Week 13: 25 June 1. Overview The final exam is a composition for instrument(s) with electronics realized in MaxMSP with occasional checkpoints where I evaluate your progress throughout the semester. For example, our Midterm Exam is one such checkpoint. Your project may be conceived as a work-in-progress but must...","url": "/final-mixed-piece/"
   },{
     "title": "Grading",
-    "excerpt":"   A list of assignments, deadlines, and points to help you plan for the semester.  For an overview of grading policies for this course, see XI. General Grading Rubric, located in the SYLLABUS.   Coming soon!       ","url": "http://localhost:4000/grading/"
+    "excerpt":"   A list of assignments, deadlines, and points to help you plan for the semester.  For an overview of grading policies for this course, see XI. General Grading Rubric, located in the SYLLABUS.   Coming soon!       ","url": "/grading/"
   },{
     "title": "Syllabus",
-    "excerpt":"1. Essential Contact Information Course Number: 00789 Semester: Fall/Winter 2023 Time: Tuesdays 14:30-16:00 Location: 2.43 Electronic Music Studio Format: Seminar Modality: In-person* * hybrid and virtual available in extenuating circumstances at the discretion of the instructor Instructor: Louis Goldford Email: Louis.Goldford@mh-luebeck.de Office Hours: Monday — Wednedsay by appointment Office Location:...","url": "http://localhost:4000/"
+    "excerpt":"1. Essential Contact Information Course Number: 00791 Semester: Summer 2024 Time: Tuesdays 14:30-16:00 Location: 2.43 Electronic Music Studio Format: Seminar Modality: In-person* Instructor: Louis Goldford Email: Louis.Goldford@mh-luebeck.de Office Hours: Monday — Wednedsay by appointment Office Location: 2.47A “The Aquarium” (next to 2.43 Electronic Music Studio) MHL Discord Server: https://discord.gg/ZQWrjtwq Course...","url": "/"
   },{
     "title": null,
-    "excerpt":"/*! * Minimal Mistakes Jekyll Theme 4.24.0 by Michael Rose * Copyright 2013-2020 Michael Rose - mademistakes.com | @mmistakes * Licensed under MIT (https://github.com/mmistakes/minimal-mistakes/blob/master/LICENSE) */ /* Variables */ /* ========================================================================== Variables ========================================================================== */ /* Typography ========================================================================== */ @import url(\"https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,400;0,600;1,100;1,400;1,600&display=swap\"); @font-face { font-family: 'Florencesans-Exp-Bold'; src: url(\"https://einbahnstrasse.github.io/MHL-Advanced-CAO/assets/fonts/Florencesans-Exp-Bold.ttf.woff\") format(\"woff\"), url(\"https://einbahnstrasse.github.io/MHL-Advanced-CAO/assets/fonts/Florencesans-Exp-Bold.ttf.svg#Florencesans-Exp-Bold\") format(\"svg\"), url(\"https://einbahnstrasse.github.io/MHL-Advanced-CAO/assets/fonts/Florencesans-Exp-Bold.ttf.eot\"), url(\"https://einbahnstrasse.github.io/MHL-Advanced-CAO/assets/fonts/Florencesans-Exp-Bold.ttf.eot?#iefix\")...","url": "http://localhost:4000/assets/main.css"
+    "excerpt":"/*! * Minimal Mistakes Jekyll Theme 4.24.0 by Michael Rose * Copyright 2013-2020 Michael Rose - mademistakes.com | @mmistakes * Licensed under MIT (https://github.com/mmistakes/minimal-mistakes/blob/master/LICENSE) */ /* Variables */ /* ========================================================================== Variables ========================================================================== */ /* Typography ========================================================================== */ @import url(\"https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,400;0,600;1,100;1,400;1,600&display=swap\"); @font-face { font-family: 'Florencesans-Exp-Bold'; src: url(\"https://einbahnstrasse.github.io/MHL-Advanced-CAO/assets/fonts/Florencesans-Exp-Bold.ttf.woff\") format(\"woff\"), url(\"https://einbahnstrasse.github.io/MHL-Advanced-CAO/assets/fonts/Florencesans-Exp-Bold.ttf.svg#Florencesans-Exp-Bold\") format(\"svg\"), url(\"https://einbahnstrasse.github.io/MHL-Advanced-CAO/assets/fonts/Florencesans-Exp-Bold.ttf.eot\"), url(\"https://einbahnstrasse.github.io/MHL-Advanced-CAO/assets/fonts/Florencesans-Exp-Bold.ttf.eot?#iefix\")...","url": "/assets/main.css"
   },{
     "title": "Software + Resources",
-    "excerpt":"A list of stuff we’ll be using throughout the course. Be sure to install + verify these applications before their respective deadlines listed on our Schedule page. MaxMSP Download + install the current build from: MaxMSP Most recent releases should support our work in class, however. At the time of...","url": "http://localhost:4000/resources/"
+    "excerpt":"A list of stuff we’ll be using throughout the course. Be sure to install + verify these applications before their respective deadlines listed on our Schedule page. MaxMSP Download + install the current build from: MaxMSP Most recent releases should support our work in class, however. At the time of...","url": "/resources/"
   },{
     "title": "Schedule",
-    "excerpt":"This page will be updated frequently with examples, video tutorials, links to new resources, and occasional updates to LAB ASSIGNMENTS. Our schedule follows the Current MHL Academic Calendar. The following topics and their precise order may change. Check here for updates! Week 1: 10 Oct. Introduction to the BACH library...","url": "http://localhost:4000/schedule/"
+    "excerpt":"This page will be updated frequently with examples, video tutorials, links to new resources, and occasional updates to LAB ASSIGNMENTS. Our schedule follows the Current MHL Academic Calendar. The following topics and their precise order may change. Check here for updates! Week 1: 2 April Introduction to the BACH library...","url": "/schedule/"
   },{
     "title": "Story Curves Project",
-    "excerpt":"Deadline: Week 6: 14 Nov. In one week! 1. Watch the Video 2. Study the Patch 3. Create a Small Étude Use the multislider object to recreate Kurt Vonnegut’s story curves, or make your own. Transform, scale, or re-calculate things based on your curve’s points by any means. Recall from...","url": "http://localhost:4000/story-curves/"
+    "excerpt":"Deadline: Week 7: 14 May 1. Watch the Video Enable German subtitles if it would help. Kurt Vonnegut (1922-2007) was an author of fiction with an engineering background, who thought mathematically about the form of his stories, which include notable works such as Slauterhouse-Five, Cat’s Cradle, Breakfast of Champions, A...","url": "/story-curves/"
   },{
     "title": "Posts by Tag",
-    "excerpt":" ","url": "http://localhost:4000/tags/"
+    "excerpt":" ","url": "/tags/"
+  },{
+    "title": "XLR Cable Coiling",
+    "excerpt":"Deadline: Week 6: 7 May Microphones and speakers are connected using 3-pin XLR cables: When it comes to wrapping cables (“coiling” or “rolling”), there are some good practice habits to form. Watch the video below for instructions on how to do this properly: By the deadline, email me a short...","url": "/xlr-cable-coiling/"
   },{
     "title": "Posts by Year",
-    "excerpt":" ","url": "http://localhost:4000/posts/"
+    "excerpt":" ","url": "/posts/"
   },{
     "title": null,
-    "excerpt":"var idx = lunr(function () { this.field('title') this.field('excerpt') this.field('categories') this.field('tags') this.ref('id') this.pipeline.remove(lunr.trimmer) for (var item in store) { this.add({ title: store[item].title, excerpt: store[item].excerpt, categories: store[item].categories, tags: store[item].tags, id: item }) } }); $(document).ready(function() { $('input#search').on('keyup', function () { var resultdiv = $('#results'); var query = $(this).val().toLowerCase(); var result = idx.query(function...","url": "http://localhost:4000/assets/js/lunr/lunr-en.js"
+    "excerpt":"var idx = lunr(function () { this.field('title') this.field('excerpt') this.field('categories') this.field('tags') this.ref('id') this.pipeline.remove(lunr.trimmer) for (var item in store) { this.add({ title: store[item].title, excerpt: store[item].excerpt, categories: store[item].categories, tags: store[item].tags, id: item }) } }); $(document).ready(function() { $('input#search').on('keyup', function () { var resultdiv = $('#results'); var query = $(this).val().toLowerCase(); var result = idx.query(function...","url": "/assets/js/lunr/lunr-en.js"
   },{
     "title": null,
-    "excerpt":"step1list = new Array(); step1list[\"ΦΑΓΙΑ\"] = \"ΦΑ\"; step1list[\"ΦΑΓΙΟΥ\"] = \"ΦΑ\"; step1list[\"ΦΑΓΙΩΝ\"] = \"ΦΑ\"; step1list[\"ΣΚΑΓΙΑ\"] = \"ΣΚΑ\"; step1list[\"ΣΚΑΓΙΟΥ\"] = \"ΣΚΑ\"; step1list[\"ΣΚΑΓΙΩΝ\"] = \"ΣΚΑ\"; step1list[\"ΟΛΟΓΙΟΥ\"] = \"ΟΛΟ\"; step1list[\"ΟΛΟΓΙΑ\"] = \"ΟΛΟ\"; step1list[\"ΟΛΟΓΙΩΝ\"] = \"ΟΛΟ\"; step1list[\"ΣΟΓΙΟΥ\"] = \"ΣΟ\"; step1list[\"ΣΟΓΙΑ\"] = \"ΣΟ\"; step1list[\"ΣΟΓΙΩΝ\"] = \"ΣΟ\"; step1list[\"ΤΑΤΟΓΙΑ\"] = \"ΤΑΤΟ\"; step1list[\"ΤΑΤΟΓΙΟΥ\"] = \"ΤΑΤΟ\"; step1list[\"ΤΑΤΟΓΙΩΝ\"] = \"ΤΑΤΟ\"; step1list[\"ΚΡΕΑΣ\"]...","url": "http://localhost:4000/assets/js/lunr/lunr-gr.js"
+    "excerpt":"step1list = new Array(); step1list[\"ΦΑΓΙΑ\"] = \"ΦΑ\"; step1list[\"ΦΑΓΙΟΥ\"] = \"ΦΑ\"; step1list[\"ΦΑΓΙΩΝ\"] = \"ΦΑ\"; step1list[\"ΣΚΑΓΙΑ\"] = \"ΣΚΑ\"; step1list[\"ΣΚΑΓΙΟΥ\"] = \"ΣΚΑ\"; step1list[\"ΣΚΑΓΙΩΝ\"] = \"ΣΚΑ\"; step1list[\"ΟΛΟΓΙΟΥ\"] = \"ΟΛΟ\"; step1list[\"ΟΛΟΓΙΑ\"] = \"ΟΛΟ\"; step1list[\"ΟΛΟΓΙΩΝ\"] = \"ΟΛΟ\"; step1list[\"ΣΟΓΙΟΥ\"] = \"ΣΟ\"; step1list[\"ΣΟΓΙΑ\"] = \"ΣΟ\"; step1list[\"ΣΟΓΙΩΝ\"] = \"ΣΟ\"; step1list[\"ΤΑΤΟΓΙΑ\"] = \"ΤΑΤΟ\"; step1list[\"ΤΑΤΟΓΙΟΥ\"] = \"ΤΑΤΟ\"; step1list[\"ΤΑΤΟΓΙΩΝ\"] = \"ΤΑΤΟ\"; step1list[\"ΚΡΕΑΣ\"]...","url": "/assets/js/lunr/lunr-gr.js"
   },{
     "title": null,
-    "excerpt":"var store = [ {%- for c in site.collections -%} {%- if forloop.last -%} {%- assign l = true -%} {%- endif -%} {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%} {%- for doc in docs -%} {%- if doc.header.teaser -%} {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture...","url": "http://localhost:4000/assets/js/lunr/lunr-store.js"
+    "excerpt":"var store = [ {%- for c in site.collections -%} {%- if forloop.last -%} {%- assign l = true -%} {%- endif -%} {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%} {%- for doc in docs -%} {%- if doc.header.teaser -%} {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture...","url": "/assets/js/lunr/lunr-store.js"
   },{
     "title": null,
-    "excerpt":"{% if page.xsl %}{% endif %}Jekyll{{ site.time | date_to_xmlschema }}{{ page.url | absolute_url | xml_escape }}{% assign title = site.title | default: site.name %}{% if page.collection != \"posts\" %}{% assign collection = page.collection | capitalize %}{% assign title = title | append: \" | \" | append: collection %}{% endif...","url": "http://localhost:4000/feed.xml"
+    "excerpt":"{% if page.xsl %}{% endif %}Jekyll{{ site.time | date_to_xmlschema }}{{ page.url | absolute_url | xml_escape }}{% assign title = site.title | default: site.name %}{% if page.collection != \"posts\" %}{% assign collection = page.collection | capitalize %}{% assign title = title | append: \" | \" | append: collection %}{% endif...","url": "/feed.xml"
   },{
     "title": null,
-    "excerpt":"{% if page.xsl %} {% endif %} {% assign collections = site.collections | where_exp:'collection','collection.output != false' %}{% for collection in collections %}{% assign docs = collection.docs | where_exp:'doc','doc.sitemap != false' %}{% for doc in docs %} {{ doc.url | replace:'/index.html','/' | absolute_url | xml_escape }} {% if doc.last_modified_at or doc.date...","url": "http://localhost:4000/sitemap.xml"
+    "excerpt":"{% if page.xsl %} {% endif %} {% assign collections = site.collections | where_exp:'collection','collection.output != false' %}{% for collection in collections %}{% assign docs = collection.docs | where_exp:'doc','doc.sitemap != false' %}{% for doc in docs %} {{ doc.url | replace:'/index.html','/' | absolute_url | xml_escape }} {% if doc.last_modified_at or doc.date...","url": "/sitemap.xml"
   },{
     "title": null,
-    "excerpt":"Sitemap: {{ \"sitemap.xml\" | absolute_url }} ","url": "http://localhost:4000/robots.txt"
+    "excerpt":"Sitemap: {{ \"sitemap.xml\" | absolute_url }} ","url": "/robots.txt"
   }]
